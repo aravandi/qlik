c76d50907c96e7fa1e57692703e0ba8f57c3d524
Amir <amirgjotbi@gmail.com>
2020-12-14 23:37:13 -0500

    the mongodb connect config

---
+++ config/mongodb.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const {
+    MONGO_USERNAME,
+    MONGO_PASSWORD,
+    MONGO_HOSTNAME,
+    MONGO_PORT,
+    MONGO_DB
+  } = process.env;
+
+const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=${MONGO_DB}`;
+
+const options = {
+    useNewUrlParser: true,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 500,
+    connectTimeoutMS: 10000,
+  };
+  
+mongoose.connect(url, options).then( function() {
+    console.log('MongoDB is connected');
+  })
+    .catch( function(err) {
+    console.log(err);
+  });
